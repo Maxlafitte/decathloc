4090d95265a9069c4f0a1a3bbf57e8b334f22732
mathilde ferrey <mathildeferrey@yahoo.com>
2019-02-18 18:53:47 +0100

    je ne sais tjs pas trop

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,1 +1,9 @@
-equipments_controller.rb
+class EquipmentsController < ApplicationController
+  def index
+    @equipments = Equipment.all
+  end
+
+  def show
+    @equipment = Equipment.find(params[:id])
+  end
+end
